693c0c36e11a63591e92a5e87d2f4aef760c72be
ffabinhoo <ffabinhoo@gmail.com>
2019-11-10 20:59:32 +1100

    convert gradle and config files

---
+++ src/main/java/ai/fabio/auction/config/AppConfig.java
@@ -0,0 +1,8 @@
+package ai.fabio.auction.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ src/main/java/ai/fabio/auction/config/DataConfig.java
@@ -0,0 +1,15 @@
+package ai.fabio.auction.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataConfig {
+
+	public DataConfig() {
+		super();
+		System.out.println("config aqui");
+	}
+	
+	
+
+}
